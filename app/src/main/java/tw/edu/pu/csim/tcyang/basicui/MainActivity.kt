104206db19a9b9f6d32f0d733837c2bb6d21b5fc
chason-cyber <chason9508@gmail.com>
2025-10-20 16:05:21 +0800

    目前播放的音樂

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -1,190 +1,216 @@
-    package tw.edu.pu.csim.tcyang.basicui
+package tw.edu.pu.csim.tcyang.basicui
 
 
-    import android.app.Activity
-    import android.media.MediaPlayer
-    import android.os.Bundle
-    import androidx.activity.ComponentActivity
-    import androidx.activity.compose.setContent
-    import androidx.activity.enableEdgeToEdge
-    import androidx.compose.foundation.BorderStroke
-    import androidx.compose.foundation.Image
-    import androidx.compose.foundation.background
-    import androidx.compose.foundation.layout.Arrangement
-    import androidx.compose.foundation.layout.Column
-    import androidx.compose.foundation.layout.Row
-    import androidx.compose.foundation.layout.Spacer
-    import androidx.compose.foundation.layout.fillMaxSize
-    import androidx.compose.foundation.layout.padding
-    import androidx.compose.foundation.layout.size
-    import androidx.compose.foundation.lazy.LazyColumn
-    import androidx.compose.foundation.lazy.LazyRow
-    import androidx.compose.foundation.shape.CircleShape
-    import androidx.compose.foundation.shape.CutCornerShape
-    import androidx.compose.material3.BottomAppBar
-    import androidx.compose.material3.Button
-    import androidx.compose.material3.ButtonDefaults
-    import androidx.compose.material3.Scaffold
-    import androidx.compose.material3.Text
-    import androidx.compose.runtime.Composable
-    import androidx.compose.runtime.mutableStateOf
-    import androidx.compose.runtime.remember
-    import androidx.compose.ui.Alignment
-    import androidx.compose.ui.Modifier
-    import androidx.compose.ui.draw.clip
-    import androidx.compose.ui.graphics.Color
-    import androidx.compose.ui.platform.LocalContext
-    import androidx.compose.ui.res.painterResource
-    import androidx.compose.ui.res.stringResource
-    import androidx.compose.ui.text.font.Font
-    import androidx.compose.ui.text.font.FontFamily
-    import androidx.compose.ui.tooling.preview.Preview
-    import androidx.compose.ui.unit.dp
-    import androidx.compose.ui.unit.sp
-    import tw.edu.pu.csim.tcyang.basicui.ui.theme.BasicUITheme
+import android.app.Activity
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import tw.edu.pu.csim.tcyang.basicui.ui.theme.BasicUITheme
 
-    class MainActivity : ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContent {
-                BasicUITheme {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        Main(
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BasicUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun Main(modifier: Modifier = Modifier) {
+@Composable
+fun Main(modifier: Modifier = Modifier) {
 
-        var Animals = listOf(R.drawable.animal0, R.drawable.animal1,
-            R.drawable.animal2, R.drawable.animal3,
-            R.drawable.animal4, R.drawable.animal5,
-            R.drawable.animal6, R.drawable.animal7,
-            R.drawable.animal8, R.drawable.animal9)
+    var Animals = listOf(R.drawable.animal0, R.drawable.animal1,
+        R.drawable.animal2, R.drawable.animal3,
+        R.drawable.animal4, R.drawable.animal5,
+        R.drawable.animal6, R.drawable.animal7,
+        R.drawable.animal8, R.drawable.animal9)
 
-        var AnimalsName = arrayListOf("鴨子","企鵝",
-            "青蛙","貓頭鷹","海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
-
-
-        // 取得當前的 Context
-        val context = LocalContext.current
+    var AnimalsName = arrayListOf("鴨子","企鵝",
+        "青蛙","貓頭鷹","海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
 
 
+    // 取得當前的 Context
+    val context = LocalContext.current
+
+    var mper: MediaPlayer? by remember { mutableStateOf(null) }
 
 
 
-        Column (
-            modifier = modifier
-                .fillMaxSize() // 1. 設定全螢幕（填滿父容器）
-                .background(Color(0xFFE0BBE4)), // 4. 設定背景為淺紫色
-            horizontalAlignment = Alignment.CenterHorizontally, // 2. 設定水平置中
-            verticalArrangement = Arrangement.Top // 3. 設定垂直靠上
-        ) {
-            Text(text = stringResource(R.string.app_title),
-                fontSize = 25.sp,
-                color = Color.Blue,
-                fontFamily = FontFamily(Font(R.font.kai))
 
+
+    Column (
+        modifier = modifier
+            .fillMaxSize() // 1. 設定全螢幕（填滿父容器）
+            .background(Color(0xFFE0BBE4)), // 4. 設定背景為淺紫色
+        horizontalAlignment = Alignment.CenterHorizontally, // 2. 設定水平置中
+        verticalArrangement = Arrangement.Top // 3. 設定垂直靠上
+    ) {
+        Text(text = stringResource(R.string.app_title),
+            fontSize = 25.sp,
+            color = Color.Blue,
+            fontFamily = FontFamily(Font(R.font.kai))
+
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Text(text = stringResource(R.string.app_author),
+            fontSize = 20.sp,
+            color = Color(0xFF654321)
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.android),
+                contentDescription = "Android 圖示",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(Color.Yellow),
+                alpha = 0.6f,
             )
 
-            Spacer(modifier = Modifier.size(10.dp))
-
-            Text(text = stringResource(R.string.app_author),
-                fontSize = 20.sp,
-                color = Color(0xFF654321)
+            Image(
+                painter = painterResource(id = R.drawable.compose),
+                contentDescription = "Compose icon",
+                modifier = Modifier.size(100.dp)
             )
 
-            Spacer(modifier = Modifier.size(10.dp))
-
-            Row {
-                Image(
-                    painter = painterResource(id = R.drawable.android),
-                    contentDescription = "Android 圖示",
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(CircleShape)
-                        .background(Color.Yellow),
-                    alpha = 0.6f,
-                    )
-
-                Image(
-                    painter = painterResource(id = R.drawable.compose),
-                    contentDescription = "Compose icon",
-                    modifier = Modifier.size(100.dp)
-                )
-
-                Image(
-                    painter = painterResource(id = R.drawable.firebase),
-                    contentDescription = "Firebase icon",
-                    modifier = Modifier.size(100.dp)
-                )
-
-            }
-
-            Spacer(modifier = Modifier.size(10.dp))
-
-            LazyRow {
-                items(51) { index ->
-                    Text(text = "$index:")
-                    Text(text = AnimalsName[index % 10])
-
-                    Image(
-                        painter = painterResource(id = Animals[index % 10]),
-                        contentDescription = "可愛動物",
-                        modifier = Modifier.size(60.dp)
-                    )
-
-                }
-            }
-
-
-            // ... 您的程式碼頂部已定義 val context = LocalContext.current
-
-// ...
-
-            Spacer(modifier = Modifier.size(10.dp))
-            Row{
-                Button(onClick = {
-                }) {
-                    Text(text = "歡迎修課")
-                }
-
-                Spacer(modifier = Modifier.size(10.dp))
-
-                Button(onClick = {
-                }) {
-                    Text(text = "展翅飛翔")
-                }
-
-                Spacer(modifier = Modifier.size(10.dp))
-
-                Button(
-                    onClick = {
-                        // 關鍵邏輯：取得 Activity 並結束它
-                        val activity = context as? Activity
-                        activity?.finish()
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00BFFF)),
-                    shape = CutCornerShape(10),
-                    border = BorderStroke(1.dp, Color.Blue),
-                    elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
-                ) {
-                    Text(text = "結束App")
-                }
-            }
-
-
-
-
-
-
-
+            Image(
+                painter = painterResource(id = R.drawable.firebase),
+                contentDescription = "Firebase icon",
+                modifier = Modifier.size(100.dp)
+            )
 
         }
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        LazyRow {
+            items(51) { index ->
+                Text(text = "$index:")
+                Text(text = AnimalsName[index % 10])
+
+                Image(
+                    painter = painterResource(id = Animals[index % 10]),
+                    contentDescription = "可愛動物",
+                    modifier = Modifier.size(60.dp)
+                )
+
+            }
+        }
+
+        Spacer(modifier = Modifier.size(10.dp))
+        Row{
+            Button(onClick = {
+                mper?.release()
+                mper = null
+                mper = MediaPlayer.create(context, R.raw.tcyang)
+                mper?.start()
+            },
+                modifier = Modifier.fillMaxWidth(0.33f).fillMaxHeight(0.8f),
+                colors = buttonColors(Color.Green)
+            ) {
+                Text(text = "歡迎", color = Color.Blue)
+                Text(text = "修課", color = Color.Red)
+                Image(
+                    painterResource(id = R.drawable.teacher),
+                    contentDescription ="teacher icon")
+
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(onClick = {
+                mper?.release()  //釋放資源
+                mper = null // 清除舊引用
+                mper = MediaPlayer.create(context, R.raw.fly) //設定音樂
+                mper?.start()  },  //開始播放
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight(0.4f),
+                colors = buttonColors(Color.Blue)
+
+            ) {
+                Text(text = "展翅飛翔", color = Color.White)
+                Image(
+                    painterResource(id = R.drawable.fly),
+                    contentDescription ="fly icon")
+
+            }
+
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(
+                onClick = {
+
+                    val activity = context as? Activity
+                    activity?.finish()
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00BFFF)),
+                shape = CutCornerShape(10),
+                border = BorderStroke(1.dp, Color.Blue),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp)
+            ) {
+                Text(text = "結束App")
+            }
+        }
+
+
+
+
+
+
     }
+}
